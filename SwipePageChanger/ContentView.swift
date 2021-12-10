65cb06c9483d7728362a37642dea4c878e88d529
Takuya Aso <milanista224@gmail.com>
2021-12-10 15:39:52 +0900

    Initial Commit

---
+++ SwipePageChanger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwipePageChanger
+//
+//  Created by Takuya Aso on 2021/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwipePageChanger/SwipePageChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipePageChangerApp.swift
+//  SwipePageChanger
+//
+//  Created by Takuya Aso on 2021/12/10.
+//
+
+import SwiftUI
+
+@main
+struct SwipePageChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
