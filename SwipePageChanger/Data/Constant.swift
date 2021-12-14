0ee88f37eadf1786625d15e1ff98cc357e93609c
Takuya Aso <milanista224@gmail.com>
2021-12-14 23:36:10 +0900

    Prepare the minimum data for the Marunouchi-Line.

--- SwipePageChanger/Data/Constant.swift
+++ SwipePageChanger/Data/Constant.swift
@@ -7,4 +7,33 @@
 
 import Foundation
 
-let marunouchi = ["荻窪", "南阿佐ケ谷", "新高円寺", "東高円寺", "新中野", "中野坂上", "西新宿", "新宿", "新宿三丁目", "新宿御苑前", "四谷三丁目", "四ツ谷", "赤坂見附", "国会議事堂前", "霞ケ関", "銀座", "東京", "大手町", "淡路町", "御茶ノ水", "本郷三丁目", "後楽園", "茗荷谷", "新大塚", "池袋"]
+let marunouchi: [(staNum: String, staName: String, staWebUrl: String)] = [
+    ("M01", "荻窪", "https://www.tokyometro.jp/station/ogikubo/index.html"),
+    ("M02", "南阿佐ケ谷", "https://www.tokyometro.jp/station/minami-asagaya/index.html"),
+    ("M03", "新高円寺", "https://www.tokyometro.jp/station/shin-koenji/index.html"),
+    ("M04", "東高円寺", "https://www.tokyometro.jp/station/higashi-koenji/index.html"),
+    ("M05", "新中野", "https://www.tokyometro.jp/station/shin-nakano/index.html"),
+    ("M06", "中野坂上", "https://www.tokyometro.jp/station/nakano-sakaue/index.html"),
+    ("M07", "西新宿", "https://www.tokyometro.jp/station/nishi-shinjuku/index.html"),
+    ("M08", "新宿", "https://www.tokyometro.jp/station/shinjuku/index.html"),
+    ("M09", "新宿三丁目", "https://www.tokyometro.jp/station/shinjuku-sanchome/index.html"),
+    ("M10", "新宿御苑前", "https://www.tokyometro.jp/station/shinjuku-gyoemmae/index.html"),
+    ("M11", "四谷三丁目", "https://www.tokyometro.jp/station/yotsuya-sanchome/index.html"),
+    ("M12", "四ツ谷", "https://www.tokyometro.jp/station/yotsuya/index.html"),
+    ("M13", "赤坂見附", "https://www.tokyometro.jp/station/akasaka-mitsuke/index.html"),
+    ("M14", "国会議事堂前", "https://www.tokyometro.jp/station/kokkai-gijidomae/index.html"),
+    ("M15", "霞ケ関", "https://www.tokyometro.jp/station/kasumigaseki/index.html"),
+    ("M16", "銀座", "https://www.tokyometro.jp/station/ginza/index.html"),
+    ("M17", "東京", "https://www.tokyometro.jp/station/tokyo/index.html"),
+    ("M18", "大手町", "https://www.tokyometro.jp/station/otemachi/index.html"),
+    ("M19", "淡路町", "https://www.tokyometro.jp/station/awajicho/index.html"),
+    ("M20", "御茶ノ水", "https://www.tokyometro.jp/station/ochanomizu/index.html"),
+    ("M21", "本郷三丁目", "https://www.tokyometro.jp/station/hongo-sanchome/index.html"),
+    ("M22", "後楽園", "https://www.tokyometro.jp/station/korakuen/index.html"),
+    ("M23", "茗荷谷", "https://www.tokyometro.jp/station/myogadani/index.html"),
+    ("M24", "新大塚", "https://www.tokyometro.jp/station/shin-otsuka/index.html"),
+    ("M25", "池袋", "https://www.tokyometro.jp/station/ikebukuro/index.html"),
+    ("Mb05", "中野新橋", "https://www.tokyometro.jp/station/nakano-shimbashi/index.html"),
+    ("Mb04", "中野富士見町", "https://www.tokyometro.jp/station/nakano-fujimicho/index.html"),
+    ("Mb03", "方南町", "https://www.tokyometro.jp/station/honancho/index.html")
+]
