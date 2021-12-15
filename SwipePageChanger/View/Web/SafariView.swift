1b14e2eca5cf9abccba9c858f3746d0bacd68a06
Takuya Aso <milanista224@gmail.com>
2021-12-15 23:16:33 +0900

    Add WebView of SafariViewController.

---
+++ SwipePageChanger/View/Web/SafariView.swift
@@ -0,0 +1,28 @@
+//
+//  SafariView.swift
+//  SwipePageChanger
+//
+//  Created by Takuya Aso on 2021/12/15.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = SFSafariViewController
+
+    var url: URL?
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url!)
+    }
+
+    func updateUIViewController(_ safariViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+    }
+}
+
+struct SafariView_Previews: PreviewProvider {
+    static var previews: some View {
+        SafariView(url: URL(string: "https://google.com/"))
+    }
+}
